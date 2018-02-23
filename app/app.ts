e9b0b7c319d336c7b3c359198adb635f59f5d983
JustinSchuyler <justin.r.schuyler@gmail.com>
2018-02-22 22:46:31 -0600

    add(solution): brute force

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,46 @@
-console.log('hello world');
+import * as _ from 'lodash';
+
+const highCoin: number = 7;
+const midCoin: number = 5;
+const lowCoin: number = 4;
+const minimumPurchase: number = 7;
+let totals: number[] = _.range(minimumPurchase, 21);
+
+totals.map(total => {
+    let highCoinCount = Math.floor(total / highCoin) - 1;
+    let midCoinCount = 0;
+    let lowCoinCount = 0;
+    let remainingTotal = total - (highCoinCount * highCoin);
+
+    if (remainingTotal % highCoin == 0) {
+        highCoinCount += 1;
+    }
+    if (remainingTotal % (2 * lowCoin) == 0) {
+        lowCoinCount += 2;
+    }
+    if (remainingTotal % (lowCoin + midCoin) == 0) {
+        lowCoinCount += 1;
+        midCoinCount += 1;
+    }
+    if (remainingTotal % (2 * midCoin) == 0) {
+        midCoinCount += 2;
+    }
+    if (remainingTotal % (lowCoin + highCoin) == 0) {
+        lowCoinCount += 1;
+        highCoinCount += 1;
+    }
+    if (remainingTotal % (midCoin + highCoin) == 0) {
+        midCoinCount += 1;
+        highCoinCount += 1;
+    }
+    if (remainingTotal % ((2 * lowCoin) + midCoin) == 0) {
+        lowCoinCount += 2;
+        midCoinCount += 1;
+    }
+
+    let results = { total, lowCoinCount, midCoinCount, highCoinCount };
+    console.log('original total:', total);
+    console.log('remaining total:', remainingTotal);
+    console.log('results:', results);
+    console.log('---');
+});
